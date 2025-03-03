c0dcf20de855ea84790034c8dd46da553e9f12f9
iMedia SysteM <imediasystem@hotmail.com>
2025-03-03 20:48:30 +0100

    First Component

---
+++ scripts.js
@@ -0,0 +1,9 @@
+var UserTable = React.createClass({
+    render: function() {
+        return (
+            <h2>To jest component UserTable</h2>
+        );
+    }
+});
+
+ReactDOM.render(<UserTable/>, document.getElementById('content'));
