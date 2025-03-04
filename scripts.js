402e7d2a07c931ce36380ee372ddf6192b72d523
iMedia SysteM <imediasystem@hotmail.com>
2025-03-04 20:49:50 +0100

    User Component Creating

--- scripts.js
+++ scripts.js
@@ -1,7 +1,109 @@
 var UserTable = React.createClass({
     render: function() {
         return (
-            <h2>To jest component UserTable</h2>
+            <div className="userTable">
+                <UserSearchBar />
+                <UserList />
+            </div>
+        );
+    }
+});
+
+var UserSearchBar = React.createClass({
+    render: function() {
+        return (
+            <div className="card userSearch">
+				<form className="form-inline">
+					<div className="form-group">
+						<input type="search" className="form-control" aria-label="userSearch" />
+						<button type="submit" className="btn btn-default">Szukaj</button>
+					</div>
+				</form>
+			</div>
+        );
+    }
+});
+
+var UserList = React.createClass({
+    render: function() {
+        return (
+            <div className="userList">
+                <UserRow1 />
+                <UserRow2 />
+                <UserRow3 />
+            </div>
+        );
+    }
+});
+
+var UserRow1 = React.createClass({
+    render: function() {
+        return (
+            <div className="card userRow">
+				<div className="row">
+					<div className="userRowLeft col-xs-2">
+						<img src="img/Foto-150.jpg" alt="avatar" className="img-responsive"/>
+					</div>
+					<div className="userRowRight col-xs-10">
+						<h4>User 1</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
+							<div className="userRowActions">
+								<span className="glyphicon glyphicon-user" aria-hidden="true"></span>
+								<span className="glyphicon glyphicon-envelope" aria-hidden="true"></span>
+								<span className="glyphicon glyphicon-pencil" aria-hidden="true"></span>
+								<span className="glyphicon glyphicon-star" aria-hidden="true"></span>
+							</div>
+					</div>
+                </div>
+            </div>
+        );
+    }
+});
+
+var UserRow2 = React.createClass({
+    render: function() {
+        return (
+            <div className="card userRow">
+				<div className="row">
+					<div className="userRowLeft col-xs-2">
+						<img src="img/aiden_pearce_face_150_white.jpg" alt="avatar" className="img-responsive"/>
+					</div>
+					<div className="userRowRight col-xs-10">
+						<h4>User 2</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
+							<div className="userRowActions">
+								<span className="glyphicon glyphicon-user" aria-hidden="true"></span>
+								<span className="glyphicon glyphicon-envelope" aria-hidden="true"></span>
+								<span className="glyphicon glyphicon-pencil" aria-hidden="true"></span>
+								<span className="glyphicon glyphicon-star" aria-hidden="true"></span>
+							</div>
+					</div>
+                </div>
+            </div>
+        );
+    }
+});
+
+var UserRow3 = React.createClass({
+    render: function() {
+        return (
+            <div className="card userRow">
+				<div className="row">
+					<div className="userRowLeft col-xs-2">
+						<img src="img/Foto_Square_150.jpg" alt="avatar" className="img-responsive"/>
+					</div>
+					<div className="userRowRight col-xs-10">
+						<h4>User 3</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
+							<div className="userRowActions">
+								<span className="glyphicon glyphicon-user" aria-hidden="true"></span>
+								<span className="glyphicon glyphicon-envelope" aria-hidden="true"></span>
+								<span className="glyphicon glyphicon-pencil" aria-hidden="true"></span>
+								<span className="glyphicon glyphicon-star" aria-hidden="true"></span>
+							</div>
+					</div>
+                </div>
+            </div>
         );
     }
 });
