21dab5c096ad7695fa76c6ab1c1399d6d5abb615
iMedia SysteM <imediasystem@hotmail.com>
2025-03-04 20:54:00 +0100

    JS modification

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 var UserTable = React.createClass({
-    render: function() {
+    render: function () {
         return (
             <div className="userTable">
                 <UserSearchBar />
@@ -10,22 +10,22 @@ var UserTable = React.createClass({
 });
 
 var UserSearchBar = React.createClass({
-    render: function() {
+    render: function () {
         return (
             <div className="card userSearch">
-				<form className="form-inline">
-					<div className="form-group">
-						<input type="search" className="form-control" aria-label="userSearch" />
-						<button type="submit" className="btn btn-default">Szukaj</button>
-					</div>
-				</form>
-			</div>
+                <form className="form-inline">
+                    <div className="form-group">
+                        <input type="search" className="form-control" aria-label="userSearch" />
+                        <button type="submit" className="btn btn-default">Szukaj</button>
+                    </div>
+                </form>
+            </div>
         );
     }
 });
 
 var UserList = React.createClass({
-    render: function() {
+    render: function () {
         return (
             <div className="userList">
                 <UserRow1 />
@@ -37,23 +37,23 @@ var UserList = React.createClass({
 });
 
 var UserRow1 = React.createClass({
-    render: function() {
+    render: function () {
         return (
             <div className="card userRow">
-				<div className="row">
-					<div className="userRowLeft col-xs-2">
-						<img src="img/Foto-150.jpg" alt="avatar" className="img-responsive"/>
-					</div>
-					<div className="userRowRight col-xs-10">
-						<h4>User 1</h4>
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
-							<div className="userRowActions">
-								<span className="glyphicon glyphicon-user" aria-hidden="true"></span>
-								<span className="glyphicon glyphicon-envelope" aria-hidden="true"></span>
-								<span className="glyphicon glyphicon-pencil" aria-hidden="true"></span>
-								<span className="glyphicon glyphicon-star" aria-hidden="true"></span>
-							</div>
-					</div>
+                <div className="row">
+                    <div className="userRowLeft col-xs-2">
+                        <img src="img/Foto-150.jpg" alt="avatar" className="img-responsive" />
+                    </div>
+                    <div className="userRowRight col-xs-10">
+                        <h4>User 1</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
+                        <div className="userRowActions">
+                            <span className="glyphicon glyphicon-user" aria-hidden="true"></span>
+                            <span className="glyphicon glyphicon-envelope" aria-hidden="true"></span>
+                            <span className="glyphicon glyphicon-pencil" aria-hidden="true"></span>
+                            <span className="glyphicon glyphicon-star" aria-hidden="true"></span>
+                        </div>
+                    </div>
                 </div>
             </div>
         );
@@ -61,23 +61,23 @@ var UserRow1 = React.createClass({
 });
 
 var UserRow2 = React.createClass({
-    render: function() {
+    render: function () {
         return (
             <div className="card userRow">
-				<div className="row">
-					<div className="userRowLeft col-xs-2">
-						<img src="img/aiden_pearce_face_150_white.jpg" alt="avatar" className="img-responsive"/>
-					</div>
-					<div className="userRowRight col-xs-10">
-						<h4>User 2</h4>
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
-							<div className="userRowActions">
-								<span className="glyphicon glyphicon-user" aria-hidden="true"></span>
-								<span className="glyphicon glyphicon-envelope" aria-hidden="true"></span>
-								<span className="glyphicon glyphicon-pencil" aria-hidden="true"></span>
-								<span className="glyphicon glyphicon-star" aria-hidden="true"></span>
-							</div>
-					</div>
+                <div className="row">
+                    <div className="userRowLeft col-xs-2">
+                        <img src="img/aiden_pearce_face_150_white.jpg" alt="avatar" className="img-responsive" />
+                    </div>
+                    <div className="userRowRight col-xs-10">
+                        <h4>User 2</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
+                        <div className="userRowActions">
+                            <span className="glyphicon glyphicon-user" aria-hidden="true"></span>
+                            <span className="glyphicon glyphicon-envelope" aria-hidden="true"></span>
+                            <span className="glyphicon glyphicon-pencil" aria-hidden="true"></span>
+                            <span className="glyphicon glyphicon-star" aria-hidden="true"></span>
+                        </div>
+                    </div>
                 </div>
             </div>
         );
@@ -85,27 +85,27 @@ var UserRow2 = React.createClass({
 });
 
 var UserRow3 = React.createClass({
-    render: function() {
+    render: function () {
         return (
             <div className="card userRow">
-				<div className="row">
-					<div className="userRowLeft col-xs-2">
-						<img src="img/Foto_Square_150.jpg" alt="avatar" className="img-responsive"/>
-					</div>
-					<div className="userRowRight col-xs-10">
-						<h4>User 3</h4>
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
-							<div className="userRowActions">
-								<span className="glyphicon glyphicon-user" aria-hidden="true"></span>
-								<span className="glyphicon glyphicon-envelope" aria-hidden="true"></span>
-								<span className="glyphicon glyphicon-pencil" aria-hidden="true"></span>
-								<span className="glyphicon glyphicon-star" aria-hidden="true"></span>
-							</div>
-					</div>
+                <div className="row">
+                    <div className="userRowLeft col-xs-2">
+                        <img src="img/Foto_Square_150.jpg" alt="avatar" className="img-responsive" />
+                    </div>
+                    <div className="userRowRight col-xs-10">
+                        <h4>User 3</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
+                        <div className="userRowActions">
+                            <span className="glyphicon glyphicon-user" aria-hidden="true"></span>
+                            <span className="glyphicon glyphicon-envelope" aria-hidden="true"></span>
+                            <span className="glyphicon glyphicon-pencil" aria-hidden="true"></span>
+                            <span className="glyphicon glyphicon-star" aria-hidden="true"></span>
+                        </div>
+                    </div>
                 </div>
             </div>
         );
     }
 });
 
-ReactDOM.render(<UserTable/>, document.getElementById('content'));
+ReactDOM.render(<UserTable />, document.getElementById('content'));
