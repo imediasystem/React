04f7206c8061b12e4dc822fd832444319fef2386
iMedia SysteM <imediasystem@hotmail.com>
2025-03-07 19:59:29 +0100

    New User Account

--- scripts.js
+++ scripts.js
@@ -31,6 +31,7 @@ var UserList = React.createClass({
                 <UserRow1 />
                 <UserRow2 />
                 <UserRow3 />
+                <UserRow4 />
             </div>
         );
     }
@@ -42,11 +43,11 @@ var UserRow1 = React.createClass({
             <div className="card userRow">
                 <div className="row">
                     <div className="userRowLeft col-xs-2">
-                        <img src="img/Foto-150.jpg" alt="avatar" className="img-responsive" />
+                        <img src="img/aiden_pearce_face_150_white.jpg" alt="avatar" className="img-responsive" />
                     </div>
                     <div className="userRowRight col-xs-10">
-                        <h4>User 1</h4>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
+                        <h4>Aiden Pearce</h4>
+                        <p>Admininstrator</p>
                         <div className="userRowActions">
                             <span className="glyphicon glyphicon-user" aria-hidden="true"></span>
                             <span className="glyphicon glyphicon-envelope" aria-hidden="true"></span>
@@ -66,11 +67,11 @@ var UserRow2 = React.createClass({
             <div className="card userRow">
                 <div className="row">
                     <div className="userRowLeft col-xs-2">
-                        <img src="img/aiden_pearce_face_150_white.jpg" alt="avatar" className="img-responsive" />
+                        <img src="img/Foto_Square_150.jpg" alt="avatar" className="img-responsive" />
                     </div>
                     <div className="userRowRight col-xs-10">
-                        <h4>User 2</h4>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
+                        <h4>Kent Butler</h4>
+                        <p>Admininstrator</p>
                         <div className="userRowActions">
                             <span className="glyphicon glyphicon-user" aria-hidden="true"></span>
                             <span className="glyphicon glyphicon-envelope" aria-hidden="true"></span>
@@ -90,11 +91,35 @@ var UserRow3 = React.createClass({
             <div className="card userRow">
                 <div className="row">
                     <div className="userRowLeft col-xs-2">
-                        <img src="img/Foto_Square_150.jpg" alt="avatar" className="img-responsive" />
+                        <img src="img/Foto-150.jpg" alt="avatar" className="img-responsive" />
                     </div>
                     <div className="userRowRight col-xs-10">
-                        <h4>User 3</h4>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
+                        <h4>Eric Dane</h4>
+                        <p>Partner</p>
+                        <div className="userRowActions">
+                            <span className="glyphicon glyphicon-user" aria-hidden="true"></span>
+                            <span className="glyphicon glyphicon-envelope" aria-hidden="true"></span>
+                            <span className="glyphicon glyphicon-pencil" aria-hidden="true"></span>
+                            <span className="glyphicon glyphicon-star" aria-hidden="true"></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+});
+
+var UserRow4 = React.createClass({
+    render: function () {
+        return (
+            <div className="card userRow">
+                <div className="row">
+                    <div className="userRowLeft col-xs-2">
+                        <img src="img/old_person_150.jpg" alt="avatar" className="img-responsive" />
+                    </div>
+                    <div className="userRowRight col-xs-10">
+                        <h4>Kris Brown</h4>
+                        <p>Partner</p>
                         <div className="userRowActions">
                             <span className="glyphicon glyphicon-user" aria-hidden="true"></span>
                             <span className="glyphicon glyphicon-envelope" aria-hidden="true"></span>
